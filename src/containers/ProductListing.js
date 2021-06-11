8b59b379661ceb0aa93f9394caa3f4dd77b4c996
Muhammad Bilal <bilal.khalid@invozone.com>
2021-06-11 17:50:26 +0500

    React Redux with Saga training complete

---
+++ src/containers/ProductListing.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import ProductComponent from "./ProductComponent";
+import axios from "axios";
+import { setProducts } from '../redux/actions/productActions';
+const ProductListing = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        fetchProducts();
+    },[])
+
+    const fetchProducts = async () => {
+        const response = await axios.get("https://fakestoreapi.com/products").catch((err) => {
+            console.log("Error", err);
+        })
+        dispatch(setProducts(response.data));
+    }
+
+    return (
+        <div className="ui grid container">
+            <ProductComponent/>
+        </div>
+    );
+}
+
+export default ProductListing;
